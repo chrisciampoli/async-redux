bfc2aca4613f7a269c461a3eff59383fb3b84797
Christopher <chris@sctr.net>
2016-11-21 10:59:24 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            {
+                test:    /\.js$/,
+                loader:  'babel-loader',
+                exclude: /node_modules/,
+                query:   {
+                    plugins: ['transform-class-properties', 'transform-decorators-legacy', 'transform-runtime'],
+                    presets: ['stage-0', 'stage-3', 'es2015', 'react']
+                }
+            }
+        ]
+    }
+};
